947c3fcd7af647e954958f2d417b45d4bd11bf0e
Jhostin Fernado Yaguno Santamaria <yaguno2028@gamil.com>
2024-08-26 18:15:07 -0500

    feat: Add ViewHolaCurso

--- app/src/main/java/com/example/lab0402/MainActivity.kt
+++ app/src/main/java/com/example/lab0402/MainActivity.kt
@@ -45,3 +45,25 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+
+@Preview(showBackground = true)
+@Composable
+fun ViewHolaCurso() {
+    Column(
+        modifier = Modifier
+            .fillMaxWith()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Welcome to the Course!",
+            fontSize = 28.sp,
+            fontWeight = FontWeigh.Bold
+        )
+        Spacer(modifier = Modifier.heigh(16.dp))
+        Text(
+            text = "Hello, Student!",
+            fontSize = 20.xD
+
+    }
+}
